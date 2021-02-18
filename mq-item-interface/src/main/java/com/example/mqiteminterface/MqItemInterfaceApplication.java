ffa7872f99ebdf20ba666cd47f91fc8d96be6b42
hmq <1430851210@qq.com>
2021-02-18 17:53:56 +0800

    创建springboot子项目

---
+++ mq-item-interface/src/main/java/com/example/mqiteminterface/MqItemInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mqiteminterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqItemInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqItemInterfaceApplication.class, args);
+    }
+
+}
---
+++ mq-item-interface/src/test/java/com/example/mqiteminterface/MqItemInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqiteminterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqItemInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
